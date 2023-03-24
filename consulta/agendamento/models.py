f33a93998ef6efc08d3b371e0641b2662c1a9395
Antonio Lindo <antoniolvitall@gmail.com>
2023-03-24 12:11:21 -0300

    cadastro de profissional

--- consulta/agendamento/models.py
+++ consulta/agendamento/models.py
@@ -1,3 +1,13 @@
 from django.db import models
 
-# Create your models here.
+
+class Profissional(models.Model):
+    id = models.CharField(max_length=5)
+    nome = models.CharField(max_length=50)
+    nomesocial = models.CharField(max_length=50)
+    cns = models.CharField(max_length=15)
+    CARGO = (
+        ('M', 'Médico'),
+        ('E', 'Enfermeiro'),
+        ('T', 'Técnico de Enfermagem')
+    )
