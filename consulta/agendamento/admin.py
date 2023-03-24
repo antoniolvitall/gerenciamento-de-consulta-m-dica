70e303cdf3f5c39391ab471891d06493997845f2
Antonio Lindo <antoniolvitall@gmail.com>
2023-03-24 15:58:33 -0300

    alterações basicas

--- consulta/agendamento/admin.py
+++ consulta/agendamento/admin.py
@@ -1,3 +1,14 @@
+from agendamento.models import Consulta, Profissional
 from django.contrib import admin
 
-# Register your models here.
+
+class Profissionais(admin.ModelAdmin):
+    list_display = ('id', 'nome', 'nomesocial','cns', 'CARGO')
+    list_display_links = ('id', 'nomesocial')
+    search_fields = ('nomesocial')
+    list_per_page = 10
+
+admin.site.register(Profissional, Profissionais)
+
+class Consultas(admin.ModelAdmin):
+    list_display = ('data_consulta')
--- consulta/agendamento/models.py
+++ consulta/agendamento/models.py
@@ -2,15 +2,17 @@ from django.db import models
 
 
 class Profissional(models.Model):
-    id = models.CharField(max_length=5)
+    matricula = models.CharField(max_length=5)
     nome = models.CharField(max_length=50)
     nomesocial = models.CharField(max_length=50)
     cns = models.CharField(max_length=15)
     CARGO = (
         ('M', 'Médico'),
         ('E', 'Enfermeiro'),
-        ('T', 'Técnico de Enfermagem')
     )
+
+    def __str__(self):
+        return self.nomesocial
 
 class Consulta(models.Model):
     data_consulta = models.DateField()
