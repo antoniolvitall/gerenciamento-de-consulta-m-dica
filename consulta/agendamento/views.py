a26e3cdee4f71db8a7c4429c2b58d2440db400ac
Antonio Lindo <antoniolvitall@gmail.com>
2023-03-24 12:38:19 -0300

    mudanças e models.py

--- consulta/agendamento/views.py
+++ consulta/agendamento/views.py
@@ -1,3 +1,6 @@
-from django.shortcuts import render
+from django.http import JsonResponse
 
-# Create your views here.
+
+def profissionais(request):
+    if request.method == 'GET':
+        profissionais = {}
